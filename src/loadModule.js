f29b661d1cf0ddcd6db51e21b31b6d95cbbc4d49
Ai-Lin Liou <alincode@gmail.com>
2019-01-22 22:56:12 +0800

    refactory load module

--- src/loadModule.js
+++ src/loadModule.js
@@ -2,16 +2,37 @@ module.exports = loadModule;
 
 // HELPER
 function loadModule(sourcecode) {
-  var script = window.document.createElement('script');
-  if ('Module' in window) {
-    var oldModule = window.Module;
-    var exists = true;
-  } else window.Module = {};
+  let script = window.document.createElement('script');
+  let exists = true;
+  if (!('Module' in window)) {
+    exists = false;
+    window.Module = {};
+  }
   script.text = `window.Module=((Module)=>{${sourcecode};return Module})()`;
   window.document.head.appendChild(script);
   window.document.head.removeChild(script);
   const compiler = window.Module;
-  if (exists) window.Module = oldModule;
-  else delete window.Module;
+  if (!exists) delete window.Module;
   return compiler;
 }
+
+// function loadModule(sourcecode) {
+//   let script = window.document.createElement('script');
+//   let oldModule, exists;
+//   if ('Module' in window) {
+//     oldModule = window.Module;
+//     exists = true;
+//   } else {
+//     window.Module = {};
+//   }
+//   script.text = `window.Module=((Module)=>{${sourcecode};return Module})()`;
+//   window.document.head.appendChild(script);
+//   window.document.head.removeChild(script);
+//   const compiler = window.Module;
+//   if (exists) {
+//     window.Module = oldModule;
+//   } else {
+//     delete window.Module;
+//   }
+//   return compiler;
+// }
